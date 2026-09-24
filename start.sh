6f387560abd0d27205bffd63263bc97c1136115a
aLfariiisi <alfarisimhmd@gmail.com>
2026-09-24 22:28:02 +0700

    Fix: Set storage permission after migrate

--- start.sh
+++ start.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# Cache konfigurasi untuk performa production
+# Cache konfigurasi
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# Jalankan migrasi ke Supabase secara otomatis
+# Jalankan migrasi
 php artisan migrate --force
+
+# -----------------------------------------------------
+# TAMBAHKAN DUA BARIS INI UNTUK MEMPERBAIKI PERMISSION:
+chown -R www-data:www-data /var/www/html/storage
+chown -R www-data:www-data /var/www/html/bootstrap/cache
+# -----------------------------------------------------
 
 # Jalankan server web Apache
 apache2-foreground
